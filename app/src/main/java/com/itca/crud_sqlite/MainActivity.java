0cc0a4676666f6ec5aeab8f270e7fd64a056c746
JennifferGzm <jennifer.gzm19@gmail.com>
2021-09-02 16:32:12 -0600

    comic

--- app/src/main/java/com/itca/crud_sqlite/MainActivity.java
+++ app/src/main/java/com/itca/crud_sqlite/MainActivity.java
@@ -17,11 +17,20 @@ import com.itca.crud_sqlite.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+
+
+    private EditText et_codigo, et_demostracion, et_presio;
+    private Button btnAlta, btnconsulta1, btnconsulta2, btneliminar, btnactualizar, btnnuevo, btnsalir;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +44,18 @@ public class MainActivity extends AppCompatActivity {
         /*NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);*/
+
+        et_codigo = findViewById(R.id.et_codigo);
+        et_demostracion = findViewById(R.id.et_demostracion);
+        et_presio = findViewById(R.id.et_presio);
+        btnAlta = findViewById(R.id.btnAlta);
+        btnconsulta1 = findViewById(R.id.btnconsulta1);
+        btnconsulta2 = findViewById(R.id.btnconsulta2);
+        btneliminar = findViewById(R.id.btneliminar);
+        btnactualizar = findViewById(R.id.btnactualizar);
+        btnnuevo = findViewById(R.id.btnnuevo);
+        btnsalir = findViewById(R.id.btnsalir);
+
 
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,9 +89,39 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+
+            case R.id.btnAlta:
+                Toast.makeText(this, "Has hecho clic en el boton Alta", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btnconsulta1:
+                Toast.makeText(this, "Has hecho clic en el boton consulta 1", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btnconsulta2:
+                Toast.makeText(this, "Has hecho clic en el boton consulta 2", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btneliminar:
+                Toast.makeText(this, "Has hecho clic en el boton eliminar", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btnactualizar:
+                Toast.makeText(this, "Has hecho clic en el boton actualizar", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btnnuevo:
+                Toast.makeText(this, "Has hecho clic en el boton nuevo", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btnsalir:
+                Toast.makeText(this, "Has hecho clic en el boton salir", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                //break;
+        }
+    }
+
+    /*@Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
-    }
+    }*/
 }
